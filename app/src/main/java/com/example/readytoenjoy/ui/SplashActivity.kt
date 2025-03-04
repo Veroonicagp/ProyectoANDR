f25fee6e2c1b482c99f6ce94afade314fb8c7613
Verónica González Pons <91873213+Veronicagnzpns@users.noreply.github.com>
2025-03-04 07:54:16 +0100

    Con Splash

---
+++ app/src/main/java/com/example/readytoenjoy/ui/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.readytoenjoy.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.readytoenjoy.R
+import com.example.readytoenjoy.ui.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            delay(2600)
+            val isUserLoggedIn = viewModel.isUserLoggedIn()
+            if (isUserLoggedIn) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            }
+            finish()
+        }
+    }
+}
